f805ee1f83deb43c8625a7f3356a702079d663cf
Abdelhamid Isam <a.isam@void.fr>
2025-08-15 12:56:46 +0100

    fix: upload file stories

--- projects/demo-app/src/app/stories/form-elements/upload-file/upload-file.stories.ts
+++ projects/demo-app/src/app/stories/form-elements/upload-file/upload-file.stories.ts
@@ -10,7 +10,57 @@ export class UploadFileStoryComponent {
   templateLink = 'assets/template.csv';
   variant: 'light' | 'dark' = 'light';
 
-  onFileSelected(file: File) {
-    console.log('form', file);
+  progressMultiple: number = 0;
+  isLoadingMultiple: boolean = false;
+
+  progressSingle: number = 0;
+  isLoadingSingle: boolean = false;
+
+  simulateUploadMultiple() {
+    const totalDurationMs = 1000;
+    const tickIntervalMs = 100;
+    const totalTicks = totalDurationMs / tickIntervalMs;
+    let ticks = 0;
+
+    this.isLoadingMultiple = true;
+    const intervalId = window.setInterval(() => {
+      ticks++;
+      this.progressMultiple = Math.round((ticks / totalTicks) * 100);
+
+
+      if (ticks >= totalTicks) {
+        clearInterval(intervalId);
+        this.progressMultiple = 100;
+        this.isLoadingMultiple = false;
+      }
+    }, tickIntervalMs);
+  }
+
+  simulateUploadSingle() {
+    const totalDurationMs = 1000;
+    const tickIntervalMs = 100;
+    const totalTicks = totalDurationMs / tickIntervalMs;
+    let ticks = 0;
+
+    this.isLoadingSingle = true;
+    const intervalId = window.setInterval(() => {
+      ticks++;
+      this.progressSingle = Math.round((ticks / totalTicks) * 100);
+
+
+      if (ticks >= totalTicks) {
+        clearInterval(intervalId);
+        this.progressSingle = 100;
+        this.isLoadingSingle = false;
+      }
+    }, tickIntervalMs);
+  }
+  
+  onFileMultipleSelected() {
+    this.simulateUploadMultiple();
+  }
+
+  onFileSingleSelected() {
+    this.simulateUploadSingle();
   }
 }
