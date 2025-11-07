0c3ef4b1dd898e3c80f7d9a7fcd485cce2b12a75
Abdelhamid Isam <a.isam@void.fr>
2025-11-07 15:58:08 +0100

    fix: range

--- projects/demo-app/src/app/stories/form-elements/range/range.stories.ts
+++ projects/demo-app/src/app/stories/form-elements/range/range.stories.ts
@@ -8,7 +8,7 @@ export class RangeStoryComponent {
   rangeValue: number = 25;
 
   myValue = 40;
-  myRange = [0, 200];
+  myRange = [0, 50000];
 
   onRangeChange(newValue: number) {
     this.rangeValue = newValue;
