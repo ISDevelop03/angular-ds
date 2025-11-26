17cf7312225cc3203ec9b46db208c23f8eafdfc2
Abdelhamid Isam <a.isam@void.fr>
2025-11-26 15:43:40 +0100

    feat: accordion documentation

--- projects/demo-app/src/app/stories/accordion/accordion.stories.ts
+++ projects/demo-app/src/app/stories/accordion/accordion.stories.ts
@@ -12,138 +12,29 @@ export class AccordionStoryComponent {
   @ViewChild('contentTemplate') contentTemplate!: TemplateRef<any>;
   @ViewChild('buttonTemplate') buttonTemplate!: TemplateRef<any>;
 
-
-  account = {
-    account_number: 'ACC-24681357',
-    balance: 15000,
-    solde_comptable: 9823479234,
-    solde_temps_reel: 20982348,
-    currency: 'USD',
-    type: 'Savings',
-    id: 'account-123',
-    date: '2025-05-02',
-    isDiscrete: false,
-  };
-
-  actionsList = [
-    {
-      label: 'Edit',
-      icon: 'pen',
-      onClick: (item: any) => alert('Edit clicked ' + JSON.stringify(item)),
-    },
-    {
-      label: 'Remove',
-      icon: 'trash',
-      onClick: (item: any) => alert('Remove clicked' + JSON.stringify(item)),
-    },
-  ];
-
-  items = [
-    {
-      label: 'Item 1',
-      value: 'Content for item 1',
-    },
-    {
-      label: 'Item 2',
-      value: `<div>
-      <h2>This is a title</h2>
-      <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. In illum ab ipsum
-    repudiandae, dolore numquam sapiente commodi ullam hic officia, ex libero
-    neque reprehenderit temporibus asperiores. Error ?
-    <strong>voluptatem id officiis</strong>
-     </p>
-    </div>`,
-    },
-    {
-      value:
-        '<h2 class="text-red-400">This is a title</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>',
-    },
-    {
-      label: 'Item 4',
-      value: 'Content for item 4',
-    },
-  ];
-
-  mockData = [
-    {
-      id: 'default-1',
-      title: 'Default Accordion 1',
-      content: [
-        {label: 'Default content one', value: 'Default content one'},
-        {label: 'Default content two', value: 'Default content two'},
-        {label: 'Default content three', value: 'Default content three'},
-        {label: 'Default content four', value: 'Default content four'},
-      ],
-      count: 5,
-      countColor: 'bg-yellow-500',
-      open: true,
-    },
-    {
-      id: 'default-2',
-      title: 'Default Accordion 1',
-      content: [
-        {label: 'Default content one', value: 'Default content one'},
-        {label: 'Default content two', value: 'Default content two'},
-      ],
-      count: 6,
-      countColor: 'bg-purple-500',
-      open: true,
-    }
-  ]
-
-  defaultItems: AccordionItem[] = [];
-  borderlessItems: AccordionItem[] = [];
-  whiteItems: AccordionItem[] = [];
+  items: AccordionItem[] = [];
+  itemsWithTemplate: AccordionItem[] = [];
+  itemsBorderless: AccordionItem[] = [];
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.defaultItems = this.mockData.map((item) => ({
-        id: item.id,
-        title: item.title,
+    this.items = [
+      {
+        id: 'default-1',
+        title: 'Default Accordion 1',
         content: this.contentTemplate,
-        context: {
-          content: item.content,
-        },
-        buttonTemplate: this.buttonTemplate,
-        count: item.count,
-        open: item.open,
-        countColor: item.countColor,
-      }));
-
-      this.whiteItems = [
-        {
-          id: 'default-1',
-          title: 'Default Accordion 1 with content',
-          content: this.templateOne,
-          count: '+9',
-        },
-        {
-          id: 'default-2',
-          title: 'Default Accordion 2 with content',
-          content: this.templateTwo,
-          count: 6,
-        },
-      ];
-
-      this.borderlessItems = [
-        {
-          id: 'borderless-1',
-          title: 'Borderless Accordion 1',
-          content: '<p>More info here</p>',
-        },
-        {
-          id: 'borderless-2',
-          title: 'Borderless Accordion 2',
-          content: '<p>Additional info</p>',
-        },
-        {
-          id: 'borderless-3',
-          title: 'Borderless Accordion 3 with template',
-          content: this.templateThree,
-        },
-      ];
-    });
+        count: 5,
+        countColor: 'bg-yellow-500',
+        open: true,
+      },
+      {
+        id: 'default-2',
+        title: 'Default Accordion 1',
+        content: this.contentTemplate,
+        count: 6,
+        countColor: 'bg-purple-500',
+        open: true,
+      },
+    ];
   }
 
   handleClick(event: Event) {
