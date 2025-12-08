75bb5ac80161d7c775ce3f85c8c5348e4a84427b
Abdelhamid Isam <a.isam@void.fr>
2025-12-08 10:56:54 +0100

    fix: range

--- projects/design-system/src/lib/components/form-elements/range/theme.ts
+++ projects/design-system/src/lib/components/form-elements/range/theme.ts
@@ -9,7 +9,7 @@ export const RangeSelectorTheme = {
   default: {
     container: 'relative w-full select-none touch-none',
     track: 'h-2 bg-neutral-50 rounded-full',
-    range: 'h-2 bg-red-600 rounded-full',
+    range: 'h-2 bg-red-500 rounded-full',
     thumb: 'w-6 h-6 bg-white rounded-full border border-red-600 shadow',
   },
 };
