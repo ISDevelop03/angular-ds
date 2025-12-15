a932df7d73aa61d7e93f8adc6224ae0766312266
Abdelhamid Isam <a.isam@void.fr>
2025-12-15 17:09:38 +0100

    fix: file list

--- projects/demo-app/src/app/stories/file-list/file-list.stories.ts
+++ projects/demo-app/src/app/stories/file-list/file-list.stories.ts
@@ -11,32 +11,72 @@ export class FileListStoryComponent {
   blob = new Blob([new Uint8Array(303039)]);
 
   ngOnInit(): void {
-    console.log('blob', Object.assign(new File([this.blob], 'test.txt', { type: 'text/plain' }), { id: 'file-1' }));
+    console.log(
+      'blob',
+      Object.assign(
+        new File([this.blob], 'Fichier des paiements en masse.txt', {
+          type: 'text/plain',
+        }),
+        { id: 'file-1' }
+      )
+    );
   }
-
 
   files: UploadedFiles[] = [
     {
-      file: Object.assign(new File([this.blob], 'test.txt', { type: 'text/plain' }), { id: 'file-1' }),
+      file: Object.assign(
+        new File([this.blob], 'Fichier des paiements en masse.txt', {
+          type: 'text/plain',
+        }),
+        { id: 'file-1' }
+      ),
       status: 'error',
     },
     {
-      file: Object.assign(new File([''], 'test.txt', { type: 'text/plain' }), { id: 'file-2' }),
+      file: Object.assign(
+        new File([''], 'Fichier des paiements en masse.txt', {
+          type: 'text/plain',
+        }),
+        {
+          id: 'file-2',
+        }
+      ),
       status: 'success',
     },
     {
-      file: Object.assign(new File([''], 'test.txt', { type: 'text/plain' }), { id: 'file-3' }),
+      file: Object.assign(
+        new File([''], 'Fichier des paiements en masse.txt', {
+          type: 'text/plain',
+        }),
+        {
+          id: 'file-3',
+        }
+      ),
       status: 'uploading',
       progress: 50,
     },
     {
-      file: Object.assign(new File([''], 'test.txt', { type: 'text/plain' }), { id: 'file-3' }),
+      file: Object.assign(
+        new File([''], 'Fichier des paiements en masse.txt', {
+          type: 'text/plain',
+        }),
+        {
+          id: 'file-3',
+        }
+      ),
     },
   ];
 
   downloadableFiles: UploadedFiles[] = [
     {
-      file: Object.assign(new File([''], 'test.txt', { type: 'text/plain' }), { id: 'file-1' }),
+      file: Object.assign(
+        new File([''], 'Fichier des paiements en masse.txt', {
+          type: 'text/plain',
+        }),
+        {
+          id: 'file-1',
+        }
+      ),
     },
   ];
 
