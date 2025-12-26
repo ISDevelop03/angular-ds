0e135127e2037229d7db8a057ff3152ae766361f
Abdelhamid Isam <a.isam@void.fr>
2025-12-26 16:06:30 +0100

    fix: table

--- projects/design-system/src/lib/components/table/theme.ts
+++ projects/design-system/src/lib/components/table/theme.ts
@@ -18,7 +18,7 @@ export interface TableTheme {
 export const TableThemes: Record<TableVariant, TableTheme> = {
   default: {
     table: 'w-full text-left border-spacing-y-1 border-separate',
-    header: 'bg-level-5',
+    header: 'bg-level-5 sticky top-0 z-10',
     headerCell: 'px-4 py-1.5 font-normal',
     headerTh: 'inline-flex items-center my-auto gap-1',
     headerText: 'inline-flex text-[0.875rem]/[1.25rem]',
@@ -31,12 +31,12 @@ export const TableThemes: Record<TableVariant, TableTheme> = {
   },
   secondary: {
     table:
-      'w-full text-left rounded-lg border border-neutral-50 border-separate border-spacing-0 overflow-hidden',
-    header: 'bg-neutral-25 rounded-t-lg',
+      'w-full text-left rounded-lg border border-neutral-50 border-separate border-spacing-0',
+    header: 'bg-neutral-25 rounded-t-lg sticky top-0 z-10',
     headerIcons: 'inline-flex items-center text-gray-500',
     headerText: 'inline-flex text-[0.875rem]/[1.125rem]',
 
-    headerCell: 'px-4 py-1.5 text-sm font-semibold',
+    headerCell: 'px-4 py-1.5 text-sm font-normal',
     headerTh: 'inline-flex items-center gap-1 text-[0.875rem]/[1.125rem]',
     headerCellButton: 'w-fit',
     headerCellSortable: ' select-none',
@@ -46,7 +46,7 @@ export const TableThemes: Record<TableVariant, TableTheme> = {
   },
   bordered: {
     table: 'w-full text-left border-collapse border',
-    header: 'bg-gray-100 border-b',
+    header: 'bg-gray-100 border-b sticky top-0 z-10',
     headerCell: 'px-4 py-2 border-r',
     headerTh: 'inline-flex items-center gap-1 text-[0.875rem]/[1.25rem]',
     headerIcons: 'inline-flex items-center text-gray-500',
