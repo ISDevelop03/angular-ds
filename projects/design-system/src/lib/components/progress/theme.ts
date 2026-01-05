fc19009dd0543ab2ac470fd4bd3f33b4107a4fd6
Abdelhamid Isam <a.isam@void.fr>
2026-01-05 16:38:12 +0100

    fix: progress

--- projects/design-system/src/lib/components/progress/theme.ts
+++ projects/design-system/src/lib/components/progress/theme.ts
@@ -3,23 +3,23 @@ export const theme = {
   variants: {
     base: 'relative overflow-hidden',
     neutral: {
-      bar: 'bg-stroke-1 h-2 w-full rounded-full',
-      progress: 'absolute left-0 top-0 bottom-0 bg-gray-500 rounded-full',
+      bar: 'bg-neutral-50 h-2 w-full rounded-full',
+      progress: 'absolute left-0 top-0 bottom-0 bg-neutral-300 rounded-full',
     },
     violet: {
-      bar: 'bg-stroke-1 h-2 w-full rounded-full',
+      bar: 'bg-neutral-50 h-2 w-full rounded-full',
       progress: 'absolute left-0 top-0 bottom-0 bg-violet-500 rounded-full',
     },
     success: {
-      bar: 'bg-stroke-1 h-2 w-full rounded-full',
+      bar: 'bg-neutral-50 h-2 w-full rounded-full',
       progress: 'absolute left-0 top-0 bottom-0 bg-success-500 rounded-full',
     },
     warning: {
-      bar: 'bg-stroke-1 h-2 w-full rounded-full',
+      bar: 'bg-neutral-50 h-2 w-full rounded-full',
       progress: 'absolute left-0 top-0 bottom-0 bg-amber-500 rounded-full',
     },
     error: {
-      bar: 'bg-stroke-1 h-2 w-full rounded-full',
+      bar: 'bg-neutral-50 h-2 w-full rounded-full',
       progress: 'absolute left-0 top-0 bottom-0 bg-error-500 rounded-full',
     },
   },
