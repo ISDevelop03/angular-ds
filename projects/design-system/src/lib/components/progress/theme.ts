76f664fc80eb7e75f94c9865d4dfdb17dccee518
Abdelhamid Isam <a.isam@void.fr>
2026-01-15 12:35:08 +0100

    fix: remove violet variant

--- projects/design-system/src/lib/components/progress/theme.ts
+++ projects/design-system/src/lib/components/progress/theme.ts
@@ -6,10 +6,6 @@ export const theme = {
       bar: 'bg-neutral-50 h-2 w-full rounded-full',
       progress: 'absolute left-0 top-0 bottom-0 bg-neutral-300 rounded-full',
     },
-    violet: {
-      bar: 'bg-neutral-50 h-2 w-full rounded-full',
-      progress: 'absolute left-0 top-0 bottom-0 bg-violet-500 rounded-full',
-    },
     success: {
       bar: 'bg-neutral-50 h-2 w-full rounded-full',
       progress: 'absolute left-0 top-0 bottom-0 bg-success-500 rounded-full',
