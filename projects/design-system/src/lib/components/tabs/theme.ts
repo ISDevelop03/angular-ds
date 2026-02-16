5956653789da8f1493ebfb952f530cec3a4aba5b
Abdelhamid Isam <a.isam@void.fr>
2026-02-16 11:30:54 +0000

    fix: tabs shadow

--- projects/design-system/src/lib/components/tabs/theme.ts
+++ projects/design-system/src/lib/components/tabs/theme.ts
@@ -40,7 +40,7 @@ export const theme = {
     list: {
       container: 'flex space-x-2 rounded bg-crame-100 p-1 w-fit',
       tab: {
-        base: 'w-fit relative text-[0.875rem]/[1.125rem] rounded py-3 px-3 focus:!outline-none !outline-none text-nowrap focus:!border-none focus:!ring-0 transition-all duration-300 ease-in-out after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:w-[70%] after:rounded-t-full after:h-[3px] after:bg-red-500 after:transition-all after:duration-300 after:ease-in-out after:opacity-0 after:content-[""]',
+        base: 'w-fit relative text-[0.875rem]/[1.125rem] rounded py-3 px-3 !shadow-none focus:!shadow-now focus:!outline-none !outline-none text-nowrap focus:!border-none focus:!ring-0 transition-all duration-300 ease-in-out after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:w-[70%] after:rounded-t-full after:h-[3px] after:bg-red-500 after:transition-all after:duration-300 after:ease-in-out after:opacity-0 after:content-[""]',
         selected: 'bg-level-3 text-neutral-900 after:opacity-100 font-bold',
         unselected:
           'bg-transparent text-neutral-900 hover:bg-level-3 hover:text-neutral-900 hover:after:opacity-100 disabled:hover:bg-transparent disabled:hover:text-neutral-500 disabled:hover:after:opacity-0',
