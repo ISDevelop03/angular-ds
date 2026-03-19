0dc4eb65fad8a6d129591e53db525d53d7b13d74
Abdelhamid Isam <a.isam@void.fr>
2026-03-19 14:04:41 +0000

    fix: modal

--- projects/design-system/src/lib/components/modal/theme.ts
+++ projects/design-system/src/lib/components/modal/theme.ts
@@ -4,5 +4,5 @@ export const theme = {
   base:
     'fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center max-w-fit',
   panel:
-    'mx-auto w-auto max-w-full h-auto overflow-hidden dark:bg-black bg-white rounded p-6 pt-7 h-fit flex items-start gap-x-5',
+    'mx-auto w-auto max-w-full h-auto overflow-hidden dark:bg-black bg-white rounded p-6 pt-7 h-fit flex gap-x-5',
 };
