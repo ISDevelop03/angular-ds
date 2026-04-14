efac4571289756563e66e28035c3e4dc1631d0af
Abdelhamid Isam <a.isam@void.fr>
2026-04-14 16:08:49 +0100

    fix: adjust tooltip colors

--- projects/design-system/src/lib/components/tooltip/theme.ts
+++ projects/design-system/src/lib/components/tooltip/theme.ts
@@ -1,6 +1,6 @@
 // theme definitions for TooltipComponent
 export const defaultTheme = {
-  base: 'w-max max-w-[300px] absolute z-[9999] px-3 py-2 text-sm font-medium text-white bg-neutral-900 dark:bg-[#000000] dark:bg-neutral-800 rounded-lg shadow-tooltip',
+  base: 'w-max max-w-[300px] absolute z-[9999] px-3 py-2 text-sm font-medium dark:text-white text-neutral-900 bg-white dark:bg-neutral-100 rounded-lg shadow-tooltip',
   positions: {
     top: 'bottom-full left-1/2 !-translate-x-1/2 mb-2',
     topRight: 'bottom-full right-0 mb-2',
@@ -14,7 +14,7 @@ export const defaultTheme = {
   arrow: {
     base: 'absolute w-0 h-0',
     positions: {
-      top: 'top-full left-1/2 !-translate-x-1/2 -translate-y-[1px] border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-neutral-900 dark:border-t-[#000000]',
+      top: 'top-full left-1/2 !-translate-x-1/2 -translate-y-[1px] border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-white dark:border-t-neutral-100',
       topRight:
         'top-full right-3 -translate-y-[1px] border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-neutral-900 dark:border-t-[#000000]',
       topLeft:
