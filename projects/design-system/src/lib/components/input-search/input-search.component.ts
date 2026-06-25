6a52c266a1d3c5c424bb6dca6a9cfc0661688c88
Abdelhamid Isam <a.isam@void.fr>
2026-06-25 16:29:17 +0100

    fix: input-search

--- projects/design-system/src/lib/components/input-search/input-search.component.ts
+++ projects/design-system/src/lib/components/input-search/input-search.component.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostListener,
   Input,
+  OnInit,
 } from '@angular/core';
 /**
  * InputSearchComponent
@@ -14,10 +15,11 @@ import {
   selector: 'ds-input-search',
   templateUrl: './input-search.component.html',
 })
-export class InputSearchComponent {
+export class InputSearchComponent implements OnInit {
   @Input() className?: string = '';
   @Input() placeholder: string = 'Search';
   @Input() maxWidth: string = '280px';
+  @Input() defaultOpen = false;
 
   @Input() value: string = '';
 
@@ -25,8 +27,18 @@ export class InputSearchComponent {
 
   constructor(private elementRef: ElementRef<HTMLElement>) { }
 
+  ngOnInit() {
+    if (this.defaultOpen) {
+      this.isExpanded = true;
+    }
+  }
+
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
+    if (this.defaultOpen) {
+      return;
+    }
+
     const target = event.target as Node;
     const clickedInside = this.elementRef.nativeElement.contains(target);
     if (!clickedInside && this.isExpanded && !this.value.trim()) {
